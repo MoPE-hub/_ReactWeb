de425ef8fa21c20ba5099bdcac21a41e8cb32462
oseun <okseongeun4@gmail.com>
2020-12-04 18:35:09 +0900

    css 물결 테스트

--- src/component/Blue.jsx
+++ src/component/Blue.jsx
@@ -3,9 +3,15 @@ import React, {Component} from 'react';
 class Blue extends Component{
     render(){
         return(
-            <div className="section_box blue">
-                testtest
+          <React.Fragment>
+            <div className="section_box">
+                <div className="section_box_inner">
+                    <div className="wave wave-1"></div>
+                    <div className="wave wave-2"></div>
+                    <div className="wave wave-3"></div>
+                </div>
             </div>
+          </React.Fragment>
         );
     }
 }
