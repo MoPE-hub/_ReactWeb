c235d3cbf65843be6e6448e38d59b63cc78fa8b4
oseun <okseongeun4@gmail.com>
2021-06-03 17:41:36 +0900

    으잉

--- src/component/animation/TextUtil.jsx
+++ src/component/animation/TextUtil.jsx
@@ -1,33 +1,35 @@
 import React, { Component } from 'react'
 
-class ButtonUtil extends Component {
+class TextUtil extends Component {
 
   render() {
 
     return(
       <React.Fragment>
-        <div>
-          <p data-text="NOB" className="text-01">
-            NOB
-          </p>
-        </div>
+        <div className="scroll-mandatory">
+          <div className="scroll">
+            <p data-text="NOB" className="text-01">
+              NOB
+            </p>
+          </div>
 
-        <div>
-          <p data-text="Neon Sign" className="text-02">
-            Neon Sign
-          </p>
-        </div>
+          <div className="scroll">
+            <p data-text="Neon Sign" className="text-02">
+              Neon Sign
+            </p>
+          </div>
 
-        <div>
-          <p data-text="Move Move" className="text-03">
-            Move Move
-          </p>
-        </div>
+          <div className="scroll">
+            <p data-text="Move Move" className="text-03">
+              Move Move
+            </p>
+          </div>
 
-        <div>
-          <p data-text="over-flip" className="text-04">
-            over-flip
-          </p>
+          {/*<div className="scroll">*/}
+          {/*  <p data-text="over-flip" className="text-04">*/}
+          {/*    over-flip*/}
+          {/*  </p>*/}
+          {/*</div>*/}
         </div>
 
       </React.Fragment>
@@ -35,4 +37,4 @@ class ButtonUtil extends Component {
   }
 }
 
-export default ButtonUtil
+export default TextUtil
