7bea122c697b843c9e1ff7406848293a5a64a6e2
oseun <okseongeun4@gmail.com>
2021-06-07 16:36:32 +0900

    AWS auth 추가

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import React from 'react'
+import React from 'react';
+import './App.css';
 import Route from './route'
 import FixBackground from "./assets/images/bg-fix.png";
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 
 function App() {
   return (
     <div className="App">
-      <Route />
-      <img src={FixBackground} alt="배경" className="background-img"/>
+      <div className="App">
+        <Route />
+        <img src={FixBackground} alt="배경" className="background-img"/>
+      </div>
+      <AmplifySignOut />
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
